7b2782d5eb478037e05da87d6eee5943e1eb35fb
Dominic Price <dominic.price@nottingham.ac.uk>
2013-01-10 14:51:30 +0000

    Removing older version before addition of new version of plugin and theme

--- WordPress/theme/artmaps/comments.php
+++
@@ -1,8 +0,0 @@
-<div id="comments">
-<?php if(have_comments()) { ?>
-    <h2 id="comments-title">Comments</h2>
-    <ol class="commentlist">
-	    <?php wp_list_comments(array("callback" => "artmapsComment")); ?>
-	</ol>
-<?php } ?>
-</div>
--- WordPress/theme/artmaps/include.php
+++
@@ -1,6 +0,0 @@
-<?php
-$trace = debug_backtrace();
-error_log("ArtMaps: File " . $trace[0]['file']
-        . " is referencing include.php");
-require_once("common.php");
-?>
