2c39538817589c734dbc4430d0e7c775fa87dcf2
Dominic Price <dominic.price@nottingham.ac.uk>
2013-01-16 16:27:06 +0000

    Added a simple tool for converting an RSA key in PEM format to a Cryptographic Service Provider blob

---
+++ PemToCspBlob/PemToCspBlob/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿#light
+
+module PemToCspBlob.Assembly
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("PemToCspBlob")>]
+[<assembly: AssemblyDescription("Console application to convert a PEM formatted RSa ")>]
+[<assembly: AssemblyCompany("Horizon Digital Economy Research, The University of Nottingham")>]
+[<assembly: AssemblyProduct("PemToCspBlob")>]
+[<assembly: AssemblyCopyright("Copyright © Horizon Digital Economy Research, The University of Nottingham, 2013")>]
+[<assembly: ComVisible(false)>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly : AssemblyInformationalVersionAttribute("1.0.0.0")>]
+do()
---
+++ PemToCspBlob/PemToCspBlob/Program.fs
@@ -0,0 +1,49 @@
+﻿#light
+
+module PemToCspBlob.Main
+
+open CommandLine
+open CommandLine.Text
+open Org.BouncyCastle.Crypto
+open Org.BouncyCastle.Crypto.Parameters
+open Org.BouncyCastle.OpenSsl
+open Org.BouncyCastle.Security
+open System
+open System.IO
+open System.Security.Cryptography
+
+type Options() = 
+    
+    [<Option("i", "in", Required = true, HelpText = "Input filename, must be a PEM formatted RSA key")>]
+    member val InputFile : string = null with get, set
+    [<Option("o", "out", Required = true, HelpText = "Output filename")>]
+    member val OutputFile : string = null with get, set
+    [<HelpOption>]
+    member this.GetUsage() =
+        HelpText.AutoBuild(this).ToString()
+
+let doConversion inf outf =
+    use r = new StreamReader(new FileStream(inf, FileMode.Open))
+    let pr = new PemReader(r)
+    let kp = pr.ReadObject() :?> AsymmetricCipherKeyPair
+    let rsa = DotNetUtilities.ToRSAParameters(kp.Private :?> RsaPrivateCrtKeyParameters);
+    let prov = new RSACryptoServiceProvider()
+    prov.PersistKeyInCsp <- false
+    prov.ImportParameters(rsa)
+    use w = new FileStream(outf, FileMode.Create)
+    let blob = prov.ExportCspBlob(true)
+    w.Write(blob, 0, blob.Length)
+
+[<EntryPoint>]
+let main argv = 
+    let parser = new CommandLineParser(new CommandLineParserSettings(Console.Error))
+    let options = new Options()
+    if parser.ParseArguments(argv, options) |> not then
+        1
+    else       
+        try   
+            doConversion options.InputFile options.OutputFile
+            0
+        with _ as e ->
+            printfn "Conversion failed with the following error: %s" e.Message
+            2
